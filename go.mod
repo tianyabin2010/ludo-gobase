81746d4324391adf025536e18130e1ca186b38ac
tianyabin <tianyabin@ixianlai.com>
2021-02-04 19:07:03 +0800

    增加mysql模块

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/Shopify/sarama v1.27.2
 	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gomodule/redigo v1.8.3
 	github.com/juju/ratelimit v1.0.1 // indirect
 	github.com/julianshen/gin-limiter v0.0.0-20161123033831-fc39b5e90fe7
--- mysql/mysql.go
+++ mysql/mysql.go
@@ -1,1 +1,21 @@
 package mysql
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/zerolog/log"
+	"time"
+)
+
+func NewMysqlClientPool(addr string, maxConn, maxIdle int) (*sql.DB, error) {
+	ret, err := sql.Open("mysql", addr)
+	if err != nil {
+		log.Error().Err(err).
+			Msgf("NewMysqlClientPool sql.Open error")
+		return nil, err
+	}
+	ret.SetConnMaxLifetime(time.Minute * 3)
+	ret.SetMaxOpenConns(maxConn)
+	ret.SetMaxIdleConns(maxIdle)
+	return ret, nil
+}
